fccd140365daff3a14b968720cd263a31d990fcf
CarpetBook <forrestlightspeed@gmail.com>
2022-11-13 21:32:41 -0600

    First release

--- gentle2lab.py
+++ gentle2lab.py
@@ -66,18 +66,66 @@ def convertFiles(fileys):
                 res = ( res, st, en )
                 lablines.append(res)
 
+            # print lab lines to a file
+            with open(str(f) + ".lab", "w") as out:
+                for line in lablines:
+                    mewhen = str(line[1]) + " " + str(line[2]) + " " + line[0]
+                    print(mewhen, file=out)
+            print("Wrote " + str(len(lablines)) + " labels to " + str(f) + ".lab")
+
         except FileNotFoundError as fe:
             print("Oops! I can't find that file. Did you type the filename correctly?")
             print(fe)
-        except JSONDecodeError as je:
+        except json.decoder.JSONDecodeError as je:
             print("Oops! The file has invalid JSON. Please check your file's formatting and try again.")
             print(je)
+        except Exception as e:
+            print("Oops! Something went wrong. Please see error below for details.")
+            print(e)
+
+def main():
+    global unkReplace
+    global notFoundReplace
+    global includeBIE
+    global neatNumbers
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--file", "-f",
+        action="extend",
+        nargs="+",
+        type=str,
+        help="input json file(s)",
+        required=True
+    )
+    parser.add_argument(
+        "--unk", "-u",
+        action="store_false",
+        help="replace unknown words with 'unk'"
+    )
+    parser.add_argument(
+        "--noreplace", "-n",
+        action="store_false",
+        help="replace unaligned words with '?'"
+    )
+    parser.add_argument(
+        "--bie", "-b",
+        action="store_true",
+        help="do not remove _B, _I, and _E"
+    )
+    parser.add_argument(
+        "--messy", "-m",
+        action="store_false",
+        help="do not round timestamps"
+    )
+    
+    args = parser.parse_args()
+    unkReplace = args.unk
+    notFoundReplace = args.noreplace
+    includeBIE = args.bie
+    neatNumbers = args.messy
+    convertFiles(args.file)
 
 if __name__ == "__main__":
-    print("gentle2lab v0.1")
-    print("usage: gentle2lab <file(s)> ...")
-    print("\noptional settings:")
-    print("--unk .......... replace unknown words with 'unk'")
-    print("--noreplace .... replace unaligned words with '?'")
-    print("--bie .......... do not remove _B, _I, and _E")
-    print("--messy ........ do not round timestamps")
+    main()
